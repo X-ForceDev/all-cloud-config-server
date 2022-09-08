a80def234446061a2d18d2f4583c2f586be218de
SUDHIR BODDU <bsudhir6@gmail.com>
2022-09-08 07:03:09 +0530

    all-configs server app completed. can be used with any application thats going to come.

---
+++ all-cloud-config-server/src/test/java/in/net/sudhir/allcloudconfigserver/AllCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.net.sudhir.allcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
